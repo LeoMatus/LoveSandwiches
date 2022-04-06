df3da09f8401f7dee6cef245bb46fa51ea8f0de0
LeoMatus <nano981023@gmail.com>
2022-04-06 02:29:10 +0000

    Calculate stock data and update stock worksheet

--- run.py
+++ run.py
@@ -74,7 +74,7 @@ def update_worksheet(data, worksheet):
     """
     Takes ints and updates the correct worksheet with the provided data
     """
-    print(f"updating the {worksheet} worksheet...")
+    print(f"Updating the {worksheet} worksheet...")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet succesfully updated\n")
@@ -91,6 +91,20 @@ def get_last_five_entries_sales():
         columns.append(column[-5:])
     return columns
 
+def calculate_stock_data(data):
+    """
+    calculate the average stock  for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
+
+
 def main():
     """
     Run all program functions
@@ -100,10 +114,13 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_five_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to love sandwitches data Automation!\n")
-#main()
+main()
 
-sales_columns = get_last_five_entries_sales()
+
 
     
